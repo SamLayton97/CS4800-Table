725e3fc2ddec66fb30e350d30f404267c7d67e0b
Sam Layton <slayton@uccs.edu>
2019-09-02 17:50:11 -0600

    Added editor object to generate object with custom tetrahedron mesh

---
+++ FractalTable/Assets/Editor/TetrahedronEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Creates a tetrahedron object in the Unity Editor
+/// </summary>
+[CustomEditor (typeof (TetrahedronMeshMaker))]
+public class TetrahedronEditor : Editor
+{
+    // Defines path to create object in editor
+    [MenuItem ("GameObject/Create Custom/Tetrahedron")]
+
+    // Called upon creation in editor
+    static void Create()
+    {
+        // create new object with mesh components
+        GameObject myObject = new GameObject("Tetrahedron");
+        TetrahedronMeshMaker tetrahedronMesh = myObject.AddComponent<TetrahedronMeshMaker>();
+        MeshFilter meshFilter = myObject.GetComponent<MeshFilter>();
+
+        // TODO: generate tetrahedron mesh
+        meshFilter.mesh = new Mesh();
+
+    }
+}
---
+++ FractalTable/Assets/Scripts/TetrahedronMeshMaker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Procedurally generates a custom tetrahedron mesh
+/// </summary>
+[RequireComponent (typeof (MeshCollider))]
+[RequireComponent (typeof (MeshFilter))]
+[RequireComponent (typeof (MeshRenderer))]
+public class TetrahedronMeshMaker : MonoBehaviour
+{
+    // Generates/Regenerates tetrahedron's custom mesh
+    public void Generate()
+    {
+        // Retrieve object's mesh filter, warning if none exist
+        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
+        if (meshFilter == null)
+        {
+            Debug.Log("Warning: Tetrahedron mesh maker lacks Mesh Filter component.");
+            return;
+        }
+
+        // define basic vertices of tetrahedron
+        Vector3 point0 = Vector3.zero;
+        Vector3 point1 = Vector3.right;
+        Vector3 point2 = new Vector3(0.5f, 0, Mathf.Sqrt(0.75f));
+        Vector3 point3 = new Vector3(0.5f, Mathf.Sqrt(0.75f), Mathf.Sqrt(0.75f) / 3);
+
+        // retrieve or make object's shared mesh
+
+    }
+}
