9f7b9af30c79f1dc3218f4b14cd20e511bdb7928
Sam Layton <slayton@uccs.edu>
2019-09-03 16:39:31 -0600

    Removed hard-coded transform values

--- FractalTable/Assets/Scripts/SierpinskiPyramidGenerator.cs
+++ FractalTable/Assets/Scripts/SierpinskiPyramidGenerator.cs
@@ -9,6 +9,7 @@ using UnityEngine;
 public class SierpinskiPyramidGenerator : MonoBehaviour
 {
     // public variables
+    [SerializeField] float precision = 0.1f;       // level of detail generation continues to until it stops
     [SerializeField] GameObject basePyramid;
     List<GameObject> pyramids;
 
@@ -41,23 +42,26 @@ public class SierpinskiPyramidGenerator : MonoBehaviour
 
     void GenerateSierpinskiPyramid(List<GameObject> toBreak)
     {
-        // create a list storing pyramids "broken" during generation
-        List<GameObject> brokenPyramids = new List<GameObject>();
-
-        // for each triangle not yet broken by current generation
-        foreach (GameObject currPyramid in toBreak)
+        // if scale of next generation doesn't exceed fractal's level of precision, continue generation
+        float newScale = toBreak[0].transform.localScale.x / 2;
+        if (newScale > precision)
         {
-            // scale current pyramid
-            currPyramid.transform.localScale *= .5f;
+            // for each triangle not yet broken by current generation
+            foreach (GameObject currPyramid in toBreak)
+            {
+                // scale current pyramid
+                currPyramid.transform.localScale *= .5f;
 
-            // duplicate/reposition pyramids to form new generation of fractal
-            // Note: Duplicate pyramids are instantiated as children of whole
-            // fractal for easy manipulation.
-            currPyramid.transform.localPosition += new Vector3(-.5f, 0, -.5f) * currPyramid.transform.localScale.x;
-            Instantiate(currPyramid, gameObject.transform).transform.localPosition += new Vector3(2.5f, 0, 2.5f);
-            Instantiate(currPyramid, gameObject.transform).transform.localPosition += new Vector3(2.5f, 0, 0);
-            Instantiate(currPyramid, gameObject.transform).transform.localPosition += new Vector3(0, 0, 2.5f);
-            Instantiate(currPyramid, gameObject.transform).transform.localPosition += new Vector3(1.25f, 2.5f * Mathf.Sqrt(.75f), 1.25f);
+                // duplicate/reposition pyramids to form new generation of fractal
+                // Note: Duplicate pyramids are instantiated as children of whole
+                // fractal for easy manipulation.
+                currPyramid.transform.localPosition += new Vector3(-.5f, 0, -.5f) * currPyramid.transform.localScale.x;
+                Instantiate(currPyramid, gameObject.transform).transform.localPosition += new Vector3(newScale, 0, newScale);
+                Instantiate(currPyramid, gameObject.transform).transform.localPosition += new Vector3(newScale, 0, 0);
+                Instantiate(currPyramid, gameObject.transform).transform.localPosition += new Vector3(0, 0, newScale);
+                Instantiate(currPyramid, gameObject.transform).transform.localPosition += 
+                    new Vector3(newScale / 2, newScale * Mathf.Sqrt(.75f), newScale / 2);
+            }
         }
     }
 }
