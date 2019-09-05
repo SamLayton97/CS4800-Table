a394271ab2f3466f1c7d9d9abb8fae379054a1f6
Sam Layton <slayton@uccs.edu>
2019-09-04 18:22:56 -0600

    Added inspector GUI function to rebuild custom mesh in editor

--- FractalTable/Assets/Editor/PyramidEditor.cs
+++ FractalTable/Assets/Editor/PyramidEditor.cs
@@ -24,4 +24,26 @@ public class PyramidEditor : Editor
         meshFilter.mesh = new Mesh();
         pyramidMesh.Generate();
     }
+
+    // Adds custom elements to components inspector GUI
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        // retrieve mesh maker component, breaking 
+        PyramidMeshMaker meshMaker = target as PyramidMeshMaker;
+        if (meshMaker == null)
+            return;
+
+        // start custom inspector GUI stuff
+        base.DrawDefaultInspector();
+        EditorGUILayout.BeginHorizontal();
+
+        // rebuild mesh when user clicks Rebuild button
+        if (GUILayout.Button("Rebuild"))
+            meshMaker.Generate();
+
+        // end custom inspector GUI stuff
+        EditorGUILayout.EndHorizontal();
+    }
 }
