422023b04656914817c50f040ffdb445d7e806ba
Sam Layton <slayton@uccs.edu>
2019-09-07 11:30:20 -0600

    User can close application by pressing the ESC key (configurable on startup)

---
+++ FractalTable/Assets/Scripts/ApplicationCloser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Closes application on user input (ESC by default)
+/// </summary>
+public class ApplicationCloser : MonoBehaviour
+{
+    // Called once per frame
+    void Update()
+    {
+        // close application when user presses quit button
+        if (Input.GetAxisRaw("Quit") != 0)
+            Application.Quit();
+    }
+}
