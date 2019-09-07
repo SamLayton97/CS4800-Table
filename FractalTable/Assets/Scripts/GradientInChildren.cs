3098887baa8be1f0e1dc273f147af28ccc051a18
Sam Layton <slayton@uccs.edu>
2019-09-06 22:53:23 -0600

    Added script to set gradient in color of parent's children
    Note: only takes two colors (start, end) and an arbitrary starting point

---
+++ FractalTable/Assets/Scripts/GradientInChildren.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls color of children's material, forming a gradient
+/// between two colors from an arbitrary point
+/// </summary>
+public class GradientInChildren : MonoBehaviour
+{
+    // public variables
+    public Color fromColor = Color.white;       // color gradient transitions from
+    public Color toColor = Color.black;         // color gradient transitions to
+    public Vector3 startPoint;                  // relative point in 3D space to set gradient away from
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
