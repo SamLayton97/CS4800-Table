f3b47ac63c083163887fbeb420eae80132356d20
Sam Layton <slayton@uccs.edu>
2019-09-07 10:51:57 -0600

    Mouse input on x-axis rotates camera around fractal's y-axis

--- FractalTable/Assets/Scripts/OrbitCamera.cs
+++ FractalTable/Assets/Scripts/OrbitCamera.cs
@@ -8,15 +8,26 @@ using UnityEngine;
 /// </summary>
 public class OrbitCamera : MonoBehaviour
 {
+    // public variables
+    public Transform targetTransform;
+    public Vector3 lookOffset;
+    public float orbitSpeed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // set camera to start facing object plus offset
+        transform.LookAt(targetTransform);
+        //transform.Rotate(lookOffset);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // rotate camera around object's y-axis using "Mouse X" input
+        transform.RotateAround(targetTransform.position, Vector3.down, Input.GetAxis("Mouse X") * Time.deltaTime * orbitSpeed);
+
+        Debug.Log("Mouse X: " + Input.GetAxis("Mouse X"));
+        //Debug.Log("Mouse Y: " + Input.GetAxis("Mouse Y"));
     }
 }
