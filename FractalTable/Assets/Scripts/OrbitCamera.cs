a9484a827a61208339b6217fe7c6c2a654911fff
Sam Layton <slayton@uccs.edu>
2019-09-07 10:30:38 -0600

    Created script to orbit around specified object, rotating with user mouse input

---
+++ FractalTable/Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Enables user to rotate camera around an object
+/// using their mouse
+/// </summary>
+public class OrbitCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
