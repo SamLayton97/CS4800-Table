5a01b672da5968f6b0847e09cc378c6ca13ad684
Sam Layton <slayton@uccs.edu>
2019-09-06 23:45:17 -0600

    User can edit radial gradient of object's children's material color

--- FractalTable/Assets/Scripts/Fractal Generators/SierpinskiTetrahedronGenerator.cs
+++ FractalTable/Assets/Scripts/Fractal Generators/SierpinskiTetrahedronGenerator.cs
@@ -13,7 +13,7 @@ public class SierpinskiTetrahedronGenerator : MonoBehaviour
     [SerializeField] GameObject baseTetrahedron;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         // if base tetrahedron wasn't set before startup
         if (baseTetrahedron == null)
--- FractalTable/Assets/Scripts/GradientInChildren.cs
+++ FractalTable/Assets/Scripts/GradientInChildren.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 /// <summary>
 /// Controls color of children's material, forming a gradient
-/// between two colors from an arbitrary point
+/// between two colors away from an arbitrary point
 /// </summary>
 public class GradientInChildren : MonoBehaviour
 {
     // public variables
     public Color fromColor = Color.white;       // color gradient transitions from
     public Color toColor = Color.black;         // color gradient transitions to
-    public Vector3 startPoint;                  // relative point in 3D space to set gradient away from
+    public Vector3 startPoint;                  // local point in 3D space to set gradient away from
+    public float radius;                        // distance to end of gradient
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        // iterate over this object's children
+        for (int currChild = 0; currChild < transform.childCount; currChild++)
+        {
+            // find distance from local start point to current child
+            Transform childTransform = transform.GetChild(currChild);
+            float distFromPoint = Vector3.Distance(childTransform.localPosition, startPoint);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            // lerp color of current child according to distance from point
+            Renderer childRend = childTransform.gameObject.GetComponent<Renderer>();
+            childRend.material.SetColor("_Color", Color.Lerp(fromColor, toColor, distFromPoint / radius));
+        }
     }
 }
